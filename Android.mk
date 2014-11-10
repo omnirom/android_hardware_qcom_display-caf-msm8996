7ed4faf581dc9bc00c4e8753a69d5b5fe8d8f21b
Steve Kondik <shade@chemlab.org>
2014-11-10 13:02:22 -0800

    Don't build lights module if vendor supplies it
    Change-Id: I63589281961e1120e7536798df6d78b8a7af4304

--- Android.mk
+++ Android.mk
@@ -4,8 +4,12 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
+endif
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
 endif
 
 display-hals += gralloc
