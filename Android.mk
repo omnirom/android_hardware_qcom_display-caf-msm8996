4dc18ecc93eb1b91810f492d14434b841a9a7f55
maxwen <max.weninger@gmail.com>
2016-10-23 18:19:41 +0200

    Add TARGET_QCOM_DISPLAY_VARIANT

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8996)
+
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -18,4 +20,6 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
 endif
