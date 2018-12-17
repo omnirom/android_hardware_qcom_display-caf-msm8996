7ffa913631842cc783c6d402ef52683a638f678e
Vachounet <vachounet@live.fr>
2018-12-17 21:30:40 +0100

    omni: add build guard
    Change-Id: I48177783798d76973a7e988dd20d405f5c8ca802

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8996)
+
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -8,10 +10,6 @@ endif
 
 display-hals += gralloc
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
+include $(call all-named-subdir-makefiles,$(display-hals))
+
 endif
